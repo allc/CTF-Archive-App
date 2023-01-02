5b0982bd2a6e25ecacf7a9abd1abb877dba5fc3e
Jinxuan <i@cjxol.com>
2023-01-02 01:38:46 +0000

    Upload github workflow file to rename config file

---
+++ webapp/src/config.deploy.js
@@ -0,0 +1,5 @@
+export const config = {
+    api_endpoint: 'https://ctf-archive-app-msirmu3nya-ew.a.run.app/api',
+    discord_client_id: '1055192988181069928',
+    discord_redirect_url: 'http://localhost:3001/auth/discord',
+};

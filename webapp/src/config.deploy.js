592f180ec49217f02506a90d2930d495dc04c313
Jinxuan <i@cjxol.com>
2023-01-02 02:08:36 +0000

    Update webapp discord redirect url

--- webapp/src/config.deploy.js
+++ webapp/src/config.deploy.js
@@ -1,5 +1,5 @@
 export const config = {
     api_endpoint: 'https://ctf-archive-app-msirmu3nya-ew.a.run.app/api',
     discord_client_id: '1055192988181069928',
-    discord_redirect_url: 'http://localhost:3001/auth/discord',
+    discord_redirect_url: 'https://ctf-archive.cjxol.com/auth/discord',
 };
